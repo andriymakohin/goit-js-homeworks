519f5cf8b599dd8a7d4447e10bdf9ab6d590f87f
Andrii Makohin <andreas2010@ukr.net>
2020-07-20 16:42:08 +0300

    hw-10end

---
+++ hw-10-food-service/src/index.js
@@ -0,0 +1,13 @@
+import menuElement from './menu.json';
+import menuTemplate from './templates/template.hbs';
+import './theme.js';
+import './styles.css';
+
+let menu = document.querySelector('.js-menu');
+
+function buildMenu(array) {
+    const markup = array.map((post) => menuTemplate(post)).join('');
+    menu.insertAdjacentHTML('beforeend', markup);
+}
+
+buildMenu(menuElement);
