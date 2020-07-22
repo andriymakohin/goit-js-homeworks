4d774244e0be700834697dbd4f96b65a6fa7bb50
Andrii Makohin <andreas2010@ukr.net>
2020-07-22 15:35:51 +0300

    hw-12end

---
+++ hw-12-countries/src/js/services/country-service.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const baseUrl = 'https://restcountries.eu/rest/v2/name/';
+
+export default {
+    fetchArticles(query) {
+        const requestParams = `${query}`;
+        return fetch(baseUrl + requestParams).then(res => res.json());
+    },
+};
